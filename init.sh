ebda4a783f27b636bd2a56df70172265a7c6f46d
Sachi854 <s1701002@sendai-nct.jp>
2021-09-12 09:16:23 +0900

    fix command

--- init.sh
+++ init.sh
@@ -4,12 +4,12 @@ sudo apt update -y
 sudo apt full-upgrade -y
 sudo apt install curl -y
 sudo apt install omxplayer -y
-mkdir ~/playlist
-curl https://raw.githubusercontent.com/Sachi854/play-video/main/play-video.sh -o ~/playlist/play-video.sh
-curl https://raw.githubusercontent.com/Sachi854/play-video/main/playlist/a.mp4 -o ~/playlist/a.mp4
-curl https://raw.githubusercontent.com/Sachi854/play-video/main/playlist/b.mp4 -o ~/playlist/b.mp4
-curl https://raw.githubusercontent.com/Sachi854/play-video/main/playlist/c.mp4 -o ~/playlist/c.mp4
-chmod +x ~/playlist/play-video.sh
-mkdir -p ~/.config/lxsession/LXDE-pi
-cp /etc/xdg/lxsession/LXDE-pi/autostart ~/.config/lxsession/LXDE-pi/
-echo "@bash ~/playlist/play-video.sh" >> ~/.config/lxsession/LXDE-pi/autostart
+mkdir $HOME/playlist
+curl https://raw.githubusercontent.com/Sachi854/play-video/main/play-video.sh -o $HOME/playlist/play-video.sh
+curl https://raw.githubusercontent.com/Sachi854/play-video/main/playlist/a.mp4 -o $HOME/playlist/a.mp4
+curl https://raw.githubusercontent.com/Sachi854/play-video/main/playlist/b.mp4 -o $HOME/playlist/b.mp4
+curl https://raw.githubusercontent.com/Sachi854/play-video/main/playlist/c.mp4 -o $HOME/playlist/c.mp4
+chmod +x $HOME/playlist/play-video.sh
+mkdir -p $HOME/.config/lxsession/LXDE-pi
+cp /etc/xdg/lxsession/LXDE-pi/autostart $HOME/.config/lxsession/LXDE-pi/
+echo "@bash ~/playlist/play-video.sh" >> $HOME/.config/lxsession/LXDE-pi/autostart
