1c8440a0624521cf0296e12127ece04b1327d494
Sachi854 <s1701002@sendai-nct.jp>
2021-09-12 10:36:06 +0900

    add exit

--- play-video.sh
+++ play-video.sh
@@ -3,15 +3,15 @@ setterm -cursor off
 VIDEOPATH="/home/pi/playlist"
 SERVICE="omxplayer"
 
+trap 'exit' SIGINT
+
 while true; do
-        if ps ax | grep -v grep | grep $SERVICE > /dev/null
-        then
-        sleep 1;
-else
-        for entry in $VIDEOPATH/*
-        do
-                clear
-                omxplayer --refresh -o both $entry > /dev/null
-        done
-fi
+        if ps ax | grep -v grep | grep $SERVICE > /dev/null; then
+        	sleep 1;
+	else
+        	for entry in $VIDEOPATH/*; do
+                	clear
+                	omxplayer --refresh -o both $entry > /dev/null
+       		done
+	fi
 done
